046b5a29eff2aea339569146a459bee0d6b403ad
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-02-09 16:10:40 +0100

    Entry Page
    The view should get the content of the encyclopedia entry by calling the appropriate util function.
    If an entry is requested that does not exist, the user should be presented with an error page indicating that their requested page was not found.
    If the entry does exist, the user should be presented with a page that displays the content of the entry. The title of the page should include the name of the entry.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
 
@@ -7,4 +7,17 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+    
+def page(request, page):
+    entry = util.get_entry(page)
+    if entry is None:
+        return render(request, "encyclopedia/not_found.html",{
+            "Title": page
+        })
+    else:
+        return render(request, "encyclopedia/pages.html",{
+                      "data": markdown2.markdown(entry),
+                      "Title": page
+                      })
+        
+    
