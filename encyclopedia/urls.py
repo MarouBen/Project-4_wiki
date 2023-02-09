f3982ad5cef43da983a0a265d870cc9e430f4f36
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-02-09 16:17:58 +0100

    Index Page
    Update index.html such that, instead of merely listing the names of all pages in the encyclopedia, user can click on any entry name to be taken directly to that entry page.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:page>",views.page, name="page")
+    path("wiki/<str:page>",views.page, name="pages")
 ]
